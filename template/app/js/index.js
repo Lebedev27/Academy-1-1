81f06d419cd7258156b212683f4c0d637c07d44b
Ilya <lebedevilya27@gmail.com>
2019-04-19 21:20:37 +0400

    адаптивный слайдер

--- template/app/js/index.js
+++ template/app/js/index.js
@@ -4,6 +4,32 @@
     var swiper = new Swiper('.item-slider__body', {
       slidesPerView: 4,
       spaceBetween: 30,
+      breakpoints: {
+        1366: {
+          slidesPerView: 3,
+          spaceBetween: 25,
+          pagination: {
+            el: '.swiper__pagination',
+            clickable: true,
+          },
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+          pagination: {
+            el: '.swiper__pagination',
+            clickable: true,
+          },
+        },
+        635: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+          pagination: {
+            el: '.swiper__pagination',
+            clickable: true,
+          },
+        },
+      },
       pagination: {
         el: '.swiper__pagination',
         clickable: true,
