5cca39fb9b12cc9f6de262ef7596fb60be63f273
Ilya <lebedevilya27@gmail.com>
2019-04-28 17:44:25 +0400

    Update gulpfile.js

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -2,10 +2,17 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
 // Static server
-gulp.task('default', function() {
+gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
 });
+gulp.task('watch', function(){
+  gulp.watch('./*html', gulp.series(browserSync.reload));
+  gulp.watch('./**/*css', gulp.series(browserSync.reload));
+});
+gulp.task('default',gulp.series(
+  gulp.parallel('browser-sync', 'watch'),
+));
