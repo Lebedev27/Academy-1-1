93d56a6931e671fdc413372bb57f17f4faf4e66d
Lebedev27 <lebedevilya27@gmail.com>
2019-05-10 23:42:14 +0400

    sass

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -1,9 +1,10 @@
 var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
+var plumber     = require('gulp-plumber');
 // Static server
 gulp.task('browser-sync', function() {
-    browserSync.init({
+    browserSync({
         server: {
             baseDir: "./"
         },
@@ -12,14 +13,14 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('sass', function() {
-  return gulp.src('app/**/*.scss')
+   return gulp.src('app/sass/**/*.scss')
+  .pipe(plumber())
   .pipe(sass({outputStyle: 'expanded'}))
   .pipe(gulp.dest('app/css'))
   .pipe(browserSync.reload({stream: true}))
 });
 gulp.task('watch', ['browser-sync', 'sass'], function() {
-    gulp.watch('app/**/*.css', browserSync.reload);
-    gulp.watch('*.html', browserSync.reload);
     gulp.watch('app/sass/**/*.scss', ['sass']);
+    gulp.watch('*.html', browserSync.reload);
 });
 gulp.task('default', ['watch']);
